2c568eba4a9d836ad74b2353a2808d6030d7228f
Karan Bhasin <karanbhasin2022@u.northwestern.edu>
2020-01-16 22:42:02 -0600

    firstMVP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
+
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
 
 const App = () => {
+  const classes = useStyles();
   const [data, setData] = useState({});
   const products = Object.values(data);
   useEffect(() => {
@@ -13,9 +35,34 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+    <Grid container spacing="4" justify="center" alignItems ='center' maxWidth="100">
+      {products.map(product => 
+        <Grid item sm pt={40} pb={40}>
+          <Card>
+            <img src={"/products/".concat(product.sku+"_1.jpg")}alt ="help"/>
+            <CardContent> 
+            <Typography key={product.sku} gutterBottom variant="h6" component="h6">
+              {product.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {"Description: ".concat(product.description)}
+            </Typography>
+            <Typography variant="body2" color="textPrimary" component="p">
+              {"$ ".concat(product.price)}
+            </Typography>
+            <ButtonGroup size="small" aria-label="small outlined button group">
+                <Button>S</Button>
+                <Button>M</Button>
+                <Button>L</Button>
+                <Button>XL</Button>
+              </ButtonGroup>
+            </CardContent>
+             
+          </Card>
+        </Grid>
+      )}
+      
+    </Grid>
   );
 };
 
